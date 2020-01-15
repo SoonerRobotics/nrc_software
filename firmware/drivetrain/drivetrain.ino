098a7787f6d122fa7255a00366a49776e0edefff
Justin Kleiber <jkleiber8@gmail.com>
2020-01-15 12:34:45 -0600

    Makes speed controllers kinda work.

--- firmware/drivetrain/drivetrain.ino
+++ firmware/drivetrain/drivetrain.ino
@@ -35,7 +35,7 @@ PIDController speedController;
 float targetSpeed = 0.0;
 
 // Velocity Estimators
-TrackingLoop left_tracking, right_tracking;
+TrackingLoop left_tracking(0.5, 5), right_tracking(0.5, 5);
 float currentSpeed;
 
 
@@ -53,10 +53,12 @@ StaticJsonDocument<128> send_pkt;
 /////////////////////////////////
 // Sensors
 /////////////////////////////////
-#define LEFT_ENC_A 0
-#define LEFT_ENC_B 0
-#define RIGHT_ENC_A 0
-#define RIGHT_ENC_B 0
+#define LEFT_ENC_A 2
+#define LEFT_ENC_B 12
+#define LEFT_TICK_CONST (float)((0.3 / 178.0))  // Rolled robot 30cm (0.3m) and got 178 ticks average, so this is meters per tick
+#define RIGHT_ENC_A 3
+#define RIGHT_ENC_B 11
+#define RIGHT_TICK_CONST (float)(-(0.3 / 178.0))// Rolled robot 30cm (0.3m) and got 178 ticks average, so this is meters per tick
 
 // BNO055
 Adafruit_BNO055 bno = Adafruit_BNO055(55);
@@ -65,6 +67,17 @@ sensors_event_t event;
 
 // Encoders
 QuadratureEncoder leftEncoder, rightEncoder;
+
+
+
+/////////////////////////////////
+// Timing
+/////////////////////////////////
+#define LOOP_RATE   200
+#define LOOP_PERIOD (float)(1.0f / (float)LOOP_RATE)
+#define MILLIS_PER_SECOND 1000
+
+unsigned long last_loop_time;
 
 
 
@@ -137,12 +150,19 @@ void setup()
     rightMotor.begin(8,7,9);
 
     // Set up the encoders
-    leftEncoder.begin(LEFT_ENC_A, LEFT_ENC_B, 1);
-    rightEncoder.begin(RIGHT_ENC_A, RIGHT_ENC_B, 1);
+    leftEncoder.begin(LEFT_ENC_A, LEFT_ENC_B, LEFT_TICK_CONST);
+    rightEncoder.begin(RIGHT_ENC_A, RIGHT_ENC_B, RIGHT_TICK_CONST);
 
     // Attach encoder interrupts
     attachInterrupt(digitalPinToInterrupt(LEFT_ENC_A), &left_encoder_isr, CHANGE);
     attachInterrupt(digitalPinToInterrupt(RIGHT_ENC_A), &right_encoder_isr, CHANGE);
+
+    // Initialize loop timer
+    last_loop_time = millis();
+
+    // Fully initialize tracking loops
+    left_tracking.reset();
+    right_tracking.reset();
 }
 
 
@@ -169,7 +189,10 @@ void loop()
     send_pkt["yaw"] = euler.x();
     send_pkt["left_vel"] = left_tracking.getVelocityEstimate();
     send_pkt["right_vel"] = right_tracking.getVelocityEstimate();
+    send_pkt["left_error"] = left_tracking.getPositionEstimate() - leftEncoder.getValue();
+    send_pkt["right_error"] = right_tracking.getPositionEstimate() - rightEncoder.getValue();
     send_pkt["acceleration"] = accel.x();
+    send_pkt["speed"] = currentSpeed;
 
     // Send data over serial
     serializeJson(send_pkt, Serial);
@@ -198,4 +221,13 @@ void loop()
     leftMotor.output(drivePower - turnPower);
     rightMotor.output(drivePower + turnPower);
 
+
+    ////////////////////////////////////////////////////////////////////////////////////////////
+
+
+    // Wait for loop update time to elapse
+    while((millis() - last_loop_time) * MILLIS_PER_SECOND < LOOP_PERIOD){}
+
+    // Update timing tracker
+    last_loop_time = millis();
 }
