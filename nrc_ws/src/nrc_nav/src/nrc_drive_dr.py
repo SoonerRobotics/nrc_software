e7cde94176c8946d43a34a237262c02fe0b9afad
Kevin Robb <kevin.robb@ou.edu>
2020-01-29 20:44:02 -0600

    finished generate_drive_command function, added global variables, and edited main. commented unnecessary imports and lines in main.

--- nrc_ws/src/nrc_nav/src/nrc_drive_dr.py
+++ nrc_ws/src/nrc_nav/src/nrc_drive_dr.py
@@ -1,18 +1,26 @@
 #! /usr/bin/env python
 
-import json
+#import json
 import rospy
-import serial
+#import serial
 
 import time
 import csv
 from numpy import genfromtxt
 
-from nrc_msgs.msg import DriveStatus, DriveCommand
+from nrc_msgs.msg import DriveCommand
 
 # Global robot objects
 robot_serial = None
-status_pub = None
+command_pub = None
+
+# global time
+start_time = time.time()
+# time index for trajectory gen instructions
+instruction_index = 0
+# instructions pulled in at the start of code running
+instructions = None
+
 
 
 # def get_drivetrain_status(timer_event):
@@ -57,53 +65,57 @@ status_pub = None
 #     robot_serial.write(command_json)
 
 
-def update_status(status_msg):
+def generate_drive_command(timer_event):
     # get local time at every tick to compare to the path-gen (in seconds)
     local_time = time.ctime(start_time)
-    
-    # csv is generated with path, based on time passed since start
-    my_data = genfromtxt('my_file.csv', delimiter=',', skipheader=1, names="time,v,accel,hdg")
-    # TODO check file to make sure column names are right
-    # TODO figure out how to actually get the csv
 
-    most_recent_time_index = round(truncate(local_time))
-    most_recent_row = my_data[most_recent_time_index]
+    # use local time to update most recent instruction
+    while instructions[instruction_index + 1][0] < local_time:
+        # if current time has passed the next instruction's time, switch "most recent" to it
+        instruction_index += 1
 
-    recent_time = most_recent_row[0]
-    vel = most_recent_row[1]
-    accel = most_recent_row[2]
-    hdg = most_recent_row[3]
+    # pull variables we need from most recent instruction
+    instruction_time = my_data[instruction_index][0]
+    # can get x and y from positions 1 and 2 when needed
+    vel = my_data[instruction_index][3]
+    accel = my_data[instruction_index][4]
+    hdg = my_data[instruction_index][5]
 
-    # use heading of most recent time in spreadsheet without interpolation
+    # use heading of most recent instruction without interpolation
     new_heading = hdg
     # calculate desired velocity by taking most recent time's velocity and interpolating with accel and time
-    new_velocity = vel + accel * (local_time - recent_time)
+    new_velocity = vel + accel * (local_time - instruction_time)
 
-    command_msg = DriveCommand()
-    command_msg.heading = new_heading
-    command_msg.speed = new_velocity
-    status_pub.publish(command_msg)
+    drive_cmd = DriveCommand()
+    drive_cmd.heading = new_heading
+    drive_cmd.speed = new_velocity
+    command_pub.publish(drive_cmd)
 
 
 if __name__ == "__main__":
-    # seconds passed since epoch
+    # seconds passed since epoch (global time, used to find local time at every call)
     start_time = time.time()
-    # use this to get local_time at every tick
+
+    # csv is generated with path, based on time passed since start
+    # TODO find exact filepath or make sure traj gen outputs the file to this directory. 
+    #       for now, I copied the file into this directory manually.
+    instructions = genfromtxt('output_traj.csv', delimiter=',', skipheader=1, names="time,x,y,velocity,accel,heading")
 
     # Initialize ROS node
     rospy.init_node("nrc_drive_dr")
 
+    # TODO does this code need robot_serial or command_sub?
     # Set up the serial communication to the robot
-    robot_serial = serial.Serial(port="/dev/ttyUSB0", baudrate=9600)
-
-    # Set up a publisher for publishing the status
-    status_pub = rospy.Publisher("/nrc/cmd", DriveCommand, queue_size=1)
+    #robot_serial = serial.Serial(port="/dev/ttyUSB0", baudrate=9600)
 
     # Set up a subscriber for getting commands
-    command_sub = rospy.Subscriber("/nrc/cmd", DriveStatus, get_drivetrain_status, queue_size=1)
+    #command_sub = rospy.Subscriber("/nrc/cmd", DriveStatus, get_drivetrain_status, queue_size=1)
+
+    # Set up a publisher for publishing the drive command
+    command_pub = rospy.Publisher("/nrc/cmd", DriveCommand, generate_drive_command, queue_size=1)
 
     # Set up a timer to read the sensor data at 200 Hz
-    update_timer = rospy.Timer(rospy.Duration(secs=0.1), update_status)
+    update_timer = rospy.Timer(rospy.Duration(secs=0.1), update_status) #TODO change update_status to generate_drive_command?
 
     # Pump callbacks
     rospy.spin()
