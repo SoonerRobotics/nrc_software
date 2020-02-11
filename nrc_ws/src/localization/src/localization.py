d322dd2fad2b9bfb071b348c0b314b781f6f93d3
ddevoreou <camper216@gmail.com>
2020-02-10 23:07:02 -0600

    localization dead reckoning

--- nrc_ws/src/localization/src/localization.py
+++ nrc_ws/src/localization/src/localization.py
@@ -3,7 +3,7 @@
 import cv2
 import numpy as np
 import rospy
-from nrc_msgs.msg import LocalizationVector
+from nrc_msgs.msg import LocalizationVector, DriveStatus
 import time
 from math import cos, radians, sin
 
@@ -14,20 +14,22 @@ from math import cos, radians, sin
 speed = 0
 heading = 0
 lastTime = 0
-coordinates = [0,0]
+coordinates = [0,-1]
+vectorPub = None
 
-def localization(sensor):
+def localization_cb(sensor):
     global speed, heading, vectorPub, coordinates, lastTime
+    print("yeet")
 
-    speed = sensor.velocity
-    heading = sensor.heading
+    speed = (sensor.right_speed + sensor.left_speed) / 2
+    heading = sensor.yaw
     elapsedTime = time.time() - lastTime
     lastTime = time.time()
 
     localization_msg = LocalizationVector()
 
-    localization_msg.x = speed * cos(heading) * elapsedTime
-    localization_msg.y = speed * sin(heading) * elapsedTime
+    localization_msg.x = speed * cos(radians(heading)) * elapsedTime
+    localization_msg.y = speed * sin(radians(heading)) * elapsedTime * -1
 
     coordinates[0] = coordinates[0] + localization_msg.x
     coordinates[1] = coordinates[1] + localization_msg.y
@@ -37,12 +39,12 @@ def localization(sensor):
 
     vectorPub.publish(localization_msg)
 
-    rospy.spin()
-
 def mainLoop():
+    global vectorPub, lastTime
+
     rospy.init_node('nrc_localization_node')
     vectorPub = rospy.Publisher('/nrc/robot_state', LocalizationVector, queue_size=10)
-    sensor = rospy.Subscriber("/nrc/sensor_data", DriveStatus, localization)
+    sensor_sub = rospy.Subscriber("/nrc/sensor_data", DriveStatus, localization_cb, queue_size=1)
     lastTime = time.time()
     
     rospy.spin()
